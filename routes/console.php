0c454ee440bee18f7a7d6be3b03ecd1b16247a7e
Rasel Mahmud Ratan <rasel.m.ratan@gmail.com>
2024-10-12 17:47:38 +0600

    scheduled at 9PM

--- routes/console.php
+++ routes/console.php
@@ -4,10 +4,10 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('send:mail')->everySecond();
+// Schedule::command('send:mail')->everySecond();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-// Schedule::command('send:mail')->dailyAt('21:00');
+Schedule::command('send:mail')->dailyAt('21:00');
